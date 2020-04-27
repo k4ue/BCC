3cab7f838016d7cf45baf4ec3c4e33375a95db60
Kauê <kaue.sbsousa@gmail.com>
2020-04-27 19:27:49 -0300

    Incluindo algorítmo de AVL

---
+++ Estrutura de Dados/Arvore AVL/Arvore AVL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Arvore AVL
+//
+//  Created by Kauê Sales on 27/04/20.
+//  Copyright © 2020 Kauê Sales. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
